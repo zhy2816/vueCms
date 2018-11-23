139741c58ead4d1653ec39c61834a013e083ec60
zhouying <zhy2816@163.com>
2018-11-23 14:54:39 +0800

    商品页面布局完成

--- src/pages/goodsList/index.vue
+++ src/pages/goodsList/index.vue
@@ -1,6 +1,89 @@
 <template>
     <div class="goodsList-container">
-        商品
+        <div class="goods-item">
+            <img src="https://i1.mifile.cn/a1/pms_1537323963.1278763!220x220.jpg" alt="">
+            <h3 class="title">小米（Mi）小米Note 16G双网通版</h3>
+            <div class="info">
+                <p class="price">
+                    <span class="now">￥899</span>
+                    <span class="old">￥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
+        <div class="goods-item">
+            <img src="https://i1.mifile.cn/a1/pms_1537323963.1278763!220x220.jpg" alt="">
+            <h3 class="title">小米（Mi）小米Note 16G双网通版</h3>
+            <div class="info">
+                <p class="price">
+                    <span class="now">￥899</span>
+                    <span class="old">￥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
+        <div class="goods-item">
+            <img src="https://i1.mifile.cn/a1/pms_1537323963.1278763!220x220.jpg" alt="">
+            <h3 class="title">小米（Mi）小米Note 16G双网通版</h3>
+            <div class="info">
+                <p class="price">
+                    <span class="now">￥899</span>
+                    <span class="old">￥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
+        <div class="goods-item">
+            <img src="https://i1.mifile.cn/a1/pms_1537323963.1278763!220x220.jpg" alt="">
+            <h3 class="title">小米（Mi）小米Note 16G双网通版</h3>
+            <div class="info">
+                <p class="price">
+                    <span class="now">￥899</span>
+                    <span class="old">￥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
+        <div class="goods-item">
+            <img src="https://i1.mifile.cn/a1/pms_1537323963.1278763!220x220.jpg" alt="">
+            <h3 class="title">小米（Mi）小米Note 16G双网通版</h3>
+            <div class="info">
+                <p class="price">
+                    <span class="now">￥899</span>
+                    <span class="old">￥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
+        <div class="goods-item">
+            <img src="https://i1.mifile.cn/a1/pms_1537323963.1278763!220x220.jpg" alt="">
+            <h3 class="title">小米（Mi）小米Note 16G双网通版</h3>
+            <div class="info">
+                <p class="price">
+                    <span class="now">￥899</span>
+                    <span class="old">￥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +94,52 @@ export default {
 </script>
 
 <style lang="less">
-
+    .goodsList-container {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 7px;
+        justify-content: space-between;
+        .goods-item{
+            width: 49%;
+            border: 1px solid #ccc;
+            box-shadow: 0 0 8px #ccc;
+            margin: 4px 0;
+            padding: 2px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            min-height: 293px;
+            img{
+                width: 100%;
+            }
+            .title{
+                font-size: 14px;
+                line-height: 20px;
+            }
+            .info{
+                background-color: #eee;
+                p{
+                    margin: 0;
+                    padding: 5px;
+                }
+                .price{
+                    .now{
+                        color: red;
+                        font-weight: bold;
+                        font-size: 16px;
+                    }
+                    .old{
+                        text-decoration: line-through;
+                        font-size: 12px;
+                        margin-left: 10px;
+                    }
+                }
+                .sell{
+                    display: flex;
+                    justify-content: space-between;
+                    font-size: 13px;
+                }
+            }
+        }
+    }
 </style>
